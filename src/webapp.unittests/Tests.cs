4c66e616e035506428f84c14c1f37c7541bd1f25
Joel Lambert <jlambert443@gmail.com>
2016-10-25 14:22:27 -0400

    commit

---
+++ src/webapp.unittests/Tests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication.Controllers;
+using Xunit;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test1() 
+        {
+            var controllerUnderTest = new HomeController();
+            var result = controllerUnderTest.Index();
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
